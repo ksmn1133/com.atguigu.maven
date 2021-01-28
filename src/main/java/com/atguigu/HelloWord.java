e49fa4067069a5369a96d1b71faa01c46bc73f1d
xiao <48642805+ksmn1133@users.noreply.github.com>
2021-01-29 00:51:55 +0800

    默认的

---
+++ src/main/java/com/atguigu/HelloWord.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author xiaoxia
+ * @create 2021/1/28 11:46 下午
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author xiaoxia
+ * @create 2021/1/28 11:47 下午
+ */
+public class HelloWorldTest {
+    @Test
+    public void testHelloWord(){
+
+    }
+}
