93f648bdd142b47a2cd29d3f7f97737a8c46e149
xiao <48642805+ksmn1133@users.noreply.github.com>
2021-01-29 18:24:54 +0800

    commit master

--- src/main/java/com/atguigu/HelloWord.java
+++ src/main/java/com/atguigu/HelloWord.java
@@ -8,6 +8,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("update one");
+        System.out.println("master one");
         System.out.println("dev one");
     }
 }
